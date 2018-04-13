26c23be5fc3ff831c4c5f6920ef13fb449752363
julia <juliabellone@gmail.com>
2018-04-13 12:30:49 +0200

    funcionando

--- src/app/_helpers/jwt.interceptor.ts
+++ src/app/_helpers/jwt.interceptor.ts
@@ -7,14 +7,17 @@ export class JwtInterceptor implements HttpInterceptor {
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // add authorization header with jwt token if available
         let currentUser = JSON.parse(localStorage.getItem('currentUser'));
-        /*if (currentUser && currentUser.token) {
+        if (request && request.url.indexOf('opentdb') > 0)
+        {
+            return next.handle(request);
+        }
+        if (currentUser && currentUser.token) {
             request = request.clone({
                 setHeaders: { 
-                    Authorization: `Bearer ${currentUser.token}`
+                    authorization: currentUser.token
                 }
             });
-        }*/
-
+        }
         return next.handle(request);
     }
 }
