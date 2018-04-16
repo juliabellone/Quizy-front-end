7fb62eb9530e1831b7fd34846044d1e5176c84f6
julia <juliabellone@gmail.com>
2018-04-16 18:15:01 +0200

    falta acabar select quiz

--- src/app/pages/selectquiz/selectquiz.component.ts
+++ src/app/pages/selectquiz/selectquiz.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizService, UserService, CreateQuizService } from '../../_services';
 import { Observable } from 'rxjs/Observable';
+import { resetFakeAsyncZone } from '@angular/core/testing';
 
 @Component({
   selector: 'app-selectquiz',
@@ -10,11 +11,11 @@ import { Observable } from 'rxjs/Observable';
 
 export class SelectQuizComponent implements OnInit {
   public isActive:string;
-  public quizes:any[];
+  public quizes;
 
   constructor(
     private quizApi: QuizService,
-    private userQuizes: CreateQuizService,
+    private userQuizesApi: CreateQuizService,
   ) { }
 
   ngOnInit() {
@@ -22,12 +23,22 @@ export class SelectQuizComponent implements OnInit {
     this.getQuizes('topics');
   }
 
+  //no mostrar todos los quizes. Hacer ngIf para mostrar los temticos o los dellos users
+
   getQuizes(source) {
     console.log(source)
     if (source == 'all') {
       this.quizApi.getCategories()
       .subscribe((response) => {
         this.quizes = response;
+        this.userQuizesApi.getAllQuizes()
+        .subscribe((response) => {
+          console.log(response);
+          response.forEach(element => {
+            this.quizes.push(element);            
+          });
+          console.log(this.quizes)
+        })
       })
       //peticion a nuestra api a la base de datos de quizes.       
         //Push a quizes con el resultado
