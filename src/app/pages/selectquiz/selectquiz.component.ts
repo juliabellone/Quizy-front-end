6b4690106fb5d6db75cce94f8c8dc09c1cb624e8
julia <juliabellone@gmail.com>
2018-04-16 18:06:30 +0200

    nos vamos acercando

--- src/app/pages/selectquiz/selectquiz.component.ts
+++ src/app/pages/selectquiz/selectquiz.component.ts
@@ -1,31 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-import { QuizService } from '../../_services';
+import { QuizService, UserService, CreateQuizService } from '../../_services';
 import { Observable } from 'rxjs/Observable';
-
 
 @Component({
   selector: 'app-selectquiz',
   templateUrl: './selectquiz.component.html',
-  styleUrls: ['./selectquiz.component.css']
+  styleUrls: ['./selectquiz.component.scss']
 })
 
 export class SelectQuizComponent implements OnInit {
-  
-  public categories:any[];
+  public isActive:string;
+  public quizes:any[];
 
   constructor(
     private quizApi: QuizService,
+    private userQuizes: CreateQuizService,
   ) { }
 
   ngOnInit() {
-    this.getCategories();
+    this.isActive = 'topics';
+    this.getQuizes('topics');
   }
 
-  getCategories() {
-    this.quizApi.getCategories()
-    .subscribe((response) => {
-      this.categories = response;
-      console.log(this.categories)
-    })
+  getQuizes(source) {
+    console.log(source)
+    if (source == 'all') {
+      this.quizApi.getCategories()
+      .subscribe((response) => {
+        this.quizes = response;
+      })
+      //peticion a nuestra api a la base de datos de quizes.       
+        //Push a quizes con el resultado
+    }
+    if (source == 'topics') {
+      this.quizApi.getCategories()
+      .subscribe((response) => {
+        this.quizes = response;
+      })
+    }
+    if (source == 'users') {
+      //peticion a nuestra api a la base de datos de quizes.       
+    }
   }
 }
