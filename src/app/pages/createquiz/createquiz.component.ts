e1a764a9a92ea24eb4f7c382c617a6ccdc76567f
julia <juliabellone@gmail.com>
2018-04-26 12:46:21 +0200

    cambios header

--- src/app/pages/createquiz/createquiz.component.ts
+++ src/app/pages/createquiz/createquiz.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-createquiz',
   templateUrl: './createquiz.component.html',
-  styleUrls: ['./createquiz.component.css']
+  styleUrls: ['./createquiz.component.scss']
 })
 export class CreateQuizComponent implements OnInit {
   
